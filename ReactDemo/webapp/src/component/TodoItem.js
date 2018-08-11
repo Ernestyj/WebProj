fb945319f14a720c22b3c70b88bdf85331c11c31
Eugene <ernestyj@outlook.com>
2018-08-12 00:26:54 +0800

    1. update ReactDemo to 16

---
+++ ReactDemo/webapp/src/component/TodoItem.js
@@ -0,0 +1,48 @@
+import React, {Component, Fragment} from 'react'
+import PropTypes from 'prop-types'
+
+
+class TodoItem extends Component {
+
+    constructor(props){
+        super(props);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+    }
+
+    componentWillReceiveProps(){
+        //若子组件从父组件接受了参数，只要父组件的render执行了，子组件此函数就会执行
+        //ex.如果此组件第一次被渲染，不会执行；之后被渲染，会执行
+        console.log('TodoItem-componentWillReceiveProps')
+    }
+
+    componentWillUnmount(){
+        console.log('TodoItem-componentWillUnmount')
+    }
+
+    render(){
+        const {content} = this.props
+        return (
+            <Fragment>
+                <li onClick={this.handleBtnClick}>{content}</li>
+            </Fragment>
+        )
+    }
+
+    handleBtnClick(e){
+        const {handleItemDelete, index} = this.props
+        handleItemDelete(index)
+    }
+
+}
+
+TodoItem.propTypes = {
+    //PropTypes.arrayOf(PropTypes.string, PropTypes.number),
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    handleItemDelete: PropTypes.func,
+    index: PropTypes.number
+}
+TodoItem.defaultProps = {
+    test: 'hello world'
+}
+
+export default TodoItem;
--- ReactDemo/webapp/src/component/TodoList.js
+++ ReactDemo/webapp/src/component/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react'
+import './TodoList.css'
+import TodoItem from './TodoItem'
 
 
 class TodoList extends Component {
@@ -9,46 +11,96 @@ class TodoList extends Component {
             inputValue: 'hello',
             list: ['Learn English', 'Learn React'],
         }
+        this.handleBtnClick = this.handleBtnClick.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleItemDelete = this.handleItemDelete.bind(this)
+    }
+
+    componentWillMount(){
+        console.log('componentWillMount')
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+
+    componentWillReceiveProps(){
+        console.log('componentWillReceiveProps')
+    }
+
+    shouldComponentUpdate(){
+        console.log('shouldComponentUpdate')
+        return true
+    }
+
+    componentWillUpdate(){
+        console.log('componentWillUpdate')
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
     }
 
     render(){
+        console.log('render')
         return (
-            <div>
-                <input onChange={this.handleInputChange.bind(this)}
-                    value={this.state.inputValue}/>
-                <button onClick={this.handleBtnClick.bind(this)}>submit</button>
-                <ul>
-                    {
-                        this.state.list.map((item, index)=>{
-                            return (
-                                <li key={index}
-                                    onClick={this.handleItemDelete.bind(this, index)}>{item}</li>
-                            )
-                        })
-                    }
+            <Fragment>
+                <label htmlFor="insertArea">Inputs: </label>
+                <input id="insertArea"
+                    onChange={this.handleInputChange}
+                    value={this.state.inputValue}
+                    className='input'
+                    ref={(input) => {this.input = input}}/>
+                <button onClick={this.handleBtnClick}>submit</button>
+                <ul ref={(ul) => {this.ul = ul}}>
+                    {this.getTodoItems()}
                 </ul>
-            </div>
+            </Fragment>
         )
     }
 
-    handleInputChange(e){
-        this.setState({
-            inputValue: e.target.value
+    getTodoItems(){
+        return this.state.list.map((item, index)=>{
+            return (
+                <Fragment key={item}>
+                    <TodoItem content={item} index={index}
+                              handleItemDelete={this.handleItemDelete}/>
+                    {/*<li key={index}*/}
+                    {/*onClick={this.handleItemDelete.bind(this, index)}*/}
+                    {/*dangerouslySetInnerHTML={{__html:item}}></li>*/}
+                </Fragment>
+            )
         })
     }
 
+    handleInputChange(e){
+        // const val = e.target.value
+        const val = this.input.value
+        this.setState(()=>({
+            inputValue: val
+        }))
+    }
+
     handleBtnClick(e){
-        this.setState({
-            list: [...this.state.list, this.state.inputValue],
+        this.setState((preState)=>({
+            list: [...preState.list, preState.inputValue],
             inputValue: ''
+        }), () => {
+            // console.log(this.ul.querySelectorAll('div').length)
         })
     }
 
     handleItemDelete(index){
-        const list = [...this.state.list];
-        list.splice(index, 1);
-        this.setState({
-            list: list
+        this.setState((preState)=>{
+            const list = [...preState.list];
+            list.splice(index, 1);
+            return {
+                list
+            }
         })
     }
 }
